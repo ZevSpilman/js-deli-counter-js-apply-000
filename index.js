5bb710ad81c7d2d85f35569b799a09064e166d69
Zev Spilman <zevspilman@yahoo.com>
2018-12-10 19:12:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(line, name){
+  line.push(name)
+  return (`Welcome, ${name}. You are number ${line.length} in line.`)
+}
+
+function nowServing(line){
+  if(line.length > 0){
+    return `Currently serving ${line.shift()}.`
+  }
+  else return 'There is nobody waiting to be served!'
+}
+
+function currentLine(line){
+  if(line.length > 0){
+    newLine = ('The line is currently')
+    for (let i = 0; i < line.length; i ++){
+      newLine += (`${i + 1}. ${name[i]}`)
+      if (i < line.length - 1){
+        newLine += (', ')
+      }
+    }
+    return newLine
+  }
+  else return 'The line is currently empty.'
+}
